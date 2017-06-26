1d81decc94ec6518e8dcf9c34d3926ad921b525e
Simon Wessel <simon.w.karlsson@gmail.com>
2017-06-26 23:49:23 +0200

    Add button functionality

--- diamond_pedestal/diamond_pedestal.ino
+++ diamond_pedestal/diamond_pedestal.ino
@@ -1,28 +1,51 @@
+unsigned long time = 0;
+byte brgtStep = 10;
+
 byte leds[] = {9}; // TODO: Add more leds
-byte brightness[] = {0};
+int brightness[] = {0};
 boolean rising[] = {true};
+boolean enabled[] = {true};
+
+byte buttons[] = {2}; // TODO: Add more buttons
+int timeouts[] = {0};
 
 void setup() {
-  // TODO: Set up input pins for buttons
+  pinMode(buttons[0], INPUT_PULLUP);
+  timeouts[0] = millis();
 }
 
 void loop() {
-  // Listen for button presses
-  // TODO
-  
-  // Update leds
-  for(int i = 0; i < sizeof(leds); i++) {
-    // write
-    analogWrite(leds[i], brightness[i]);
-    // update brightness
-    if(rising[i]) {
-      brightness[i]++;
-    } else {
-      brightness[i]--;
-    }
-    if(brightness[i] >= 255 || brightness[i] <= 0) {
-      rising[i] = !rising[i];
+  // Some debounce magic to clean up button input.
+  for(int i = 0; i < (sizeof(timeouts)/sizeof(int)); i++) {
+    if((millis() - timeouts[i]) >= 200) {
+      int buttonState = digitalRead(buttons[i]);
+        if(buttonState == LOW) {
+          enabled[i] = !enabled[i];
+          timeouts[i] = millis();
+        }
     }
   }
-  delay(5);
+  
+  // Update leds
+  if(millis() - time >= 50) {
+    for(int i = 0; i < sizeof(leds); i++) {
+      if(enabled[i]) {
+        analogWrite(leds[i], brightness[i]);
+        if(rising[i]) {
+          brightness[i] = brightness[i] + brgtStep;
+          if(brightness[i] > 255) brightness[i] = 255;
+        } else {
+          brightness[i] = brightness[i] - brgtStep;
+          if(brightness[i] < 0) brightness[i] = 0;
+        }
+        if(brightness[i] == 255 || brightness[i] == 0) {
+          rising[i] = !rising[i];
+        }
+      } else {
+        analogWrite(leds[i], 0);
+        brightness[i] = 0;
+      }
+    }
+    time = millis();
+  }
 }
